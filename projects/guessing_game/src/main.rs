f14f8ab497d986b8e15333745248d84dde909fcf
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-04-03 13:26:16 +0000

    feat: parse & match

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,17 +1,28 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
 
-    let chosen_number = rand::thread_rng().gen_range(1..=10);
-
     let mut guess = String::new();
+
+    let chosen_number = rand::thread_rng().gen_range(1..=10);
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("Number is {chosen_number}, You guessed: {guess}");
+    let guess_number: i32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => -1,
+    };
+
+    match guess_number.cmp(&chosen_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
+
