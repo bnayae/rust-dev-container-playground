aa92d8b285a150584a8ee1f8ccafca8ec742bd46
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-04-08 19:51:08 +0000

    feat: loops

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -4,25 +4,32 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let v = vec![1, 2, 3];
 
-    let chosen_number = rand::thread_rng().gen_range(1..=10);
+    loop {
+        let chosen_number = rand::thread_rng().gen_range(1..=10);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        for _ in &v {
+            let mut guess = String::new(); // MUST BE INSIDE THE LOOP
 
-    let guess_number: i32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => -1,
-    };
+            println!("Please input your guess.");
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
+            let guess_number: i32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => -1,
+            };
 
-    match guess_number.cmp(&chosen_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+            match guess_number.cmp(&chosen_number) {
+                Ordering::Less => println!("Too small!, for '{guess_number}'"),
+                Ordering::Greater => println!("Too big!,  for '{guess_number}'"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
+            }
+        }
     }
 }
-
--- projects/hello_cargo/src/main.rs
+++ projects/hello_cargo/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, cargo!");
+
+    let vector = vec!["a", "b", "c"];
+    println!("For loop");
+    for x in vector {
+        // consume vector, owned x
+        println!("\tItem = {x}");
+    }
+
+    println!("is actually: iter + while");
+    // todo: let mut iter = vector.into_iter();
+    let mut iter = vec!["a", "b", "c"].into_iter();
+    while let Some(e) = iter.next() {
+        println!("\tItem = {e}");
+    }
+
+    // https://www.youtube.com/watch?v=yozQ9C69pNs&list=PLqbS7AVVErFiWDOAVrPt7aYmnuuOLYvOa&index=3
+    println!("For loop + .iter");
+    for x in vector.into_iter() {
+        // borrows vector, & to x
+        println!("\tItem = {x}");
+    }
+
+    // todo: fix ^^^^ value borrowed here after move
+    // println!("For &loop");
+    // for x in &vector {
+    //     // equivalent to vector.into_iter()
+    //     println!("\tItem = {x}");
+    // }
 }
