e6f1b2265266b4c935eb4c56f31d414efe14465b
Bnaya Eshet <3382669+bnayae@users.noreply.github.com>
2023-04-12 09:29:36 +0000

    feat: slice

--- projects/why_ownership/src/main.rs
+++ projects/why_ownership/src/main.rs
@@ -1,12 +1,18 @@
 fn main() {
-    let mut s = String::from("hello world");
-    // let word = first_word(&s); 
-    let word = first_word_slice(&s); 
-    // s.clear(); 
+    let mut s = String::from("hello world ");
+    let word = first_word(&s);
+    // let word = first_word_slice(&s);
+    s.clear();
     println!("'{s}' starts with '{word}'");
+    let word = rest_slice("hello world");
+    println!("'hello world' ends with '{word}'");
+    let word = rest_slice("hello ");
+    println!("'hello ' ends with '{word}'");
+    let word = rest_slice("hello-world ");
+    println!("'hello-world' ends with '{word}'");
 }
 
-fn first_word (source: &str) -> String {
+fn first_word(source: &str) -> String {
     let mut result = String::new();
     for c in source.chars() {
         if c == ' ' {
@@ -17,11 +23,20 @@ fn first_word (source: &str) -> String {
     result
 }
 
-fn first_word_slice (source: &str) -> &str {
+fn first_word_slice(source: &str) -> &str {
     for (i, c) in source.chars().into_iter().enumerate() {
         if c == ' ' {
             return &source[..i];
         }
     }
     &source[..]
+}
+
+fn rest_slice(source: &str) -> &str {
+    for (i, c) in source.chars().into_iter().enumerate() {
+        if c == ' ' {
+            return &source[i + 1..];
+        }
+    }
+    ""
 }
